f6878eedc4a90382660b50386bf77da255083be4
shinantc <shinantc@croswer.com>
2022-04-23 16:29:33 +0530

    Admin Login Successfull

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
+const { PrismaClient } = require('@prisma/client');
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
+
+const prisma = new PrismaClient();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
@@ -9,5 +13,26 @@ router.get('/', (req, res, next) => {
 router.get('/login', (req, res, next) => {
   res.render('admin/admin-login');
 });
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await prisma.Admin.findMany({
+      where: {
+        name: username,
+        password
+      }
+    });
+
+    if (user.length == 0)
+      console.log("Authentication Failed");
+    else {
+      res.status(200).render('admin/admin-home');
+      console.log("Authentication Success");
+    }
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
